8c46e9500921096e81e5020927195889cedcbaf0
s0nyseventeen <sonysevent@gmail.com>
2021-02-18 15:28:43 +0200

    18/02 registration form: OK

--- django_project/settings.py
+++ django_project/settings.py
@@ -39,6 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # addition
+    'crispy_forms',
+
     # own
     'blog.apps.BlogConfig',
     'users.apps.UsersConfig',
@@ -123,3 +126,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
