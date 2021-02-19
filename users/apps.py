5cc3c35995a4d808db6261ae72ff11a2d74d87fe
s0nyseventeen <sonysevent@gmail.com>
2021-02-20 00:19:27 +0200

    20.02 Signals Profile OK

--- users/apps.py
+++ users/apps.py
@@ -3,3 +3,7 @@ from django.apps import AppConfig
 
 class UsersConfig(AppConfig):
     name = 'users'
+
+    # to avoid creating Profiles through admin panel
+    def ready(self):
+    	import users.signals
