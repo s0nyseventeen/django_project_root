e144ba612a5c77a66ccf8906084b0de4cc902a85
s0nyseventeen <sonysevent@gmail.com>
2021-02-20 11:40:07 +0200

    20/02 Generic, post-detail - OK

--- blog/urls.py
+++ blog/urls.py
@@ -5,6 +5,10 @@ from . import views
 app_name = 'blog'
 
 urlpatterns = [
-	path('', views.home, name='blog-home'),
+	# func views
+	# path('', views.home, name='blog-home'),
 	path('about/', views.about, name='blog-about'),
+	# generic views
+	path('', views.PostListView.as_view(), name='blog-home'),
+	path('post/<int:pk>/', views.PostDetailView.as_view(), name='post-detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,30 @@
 from django.shortcuts import render
+from django.views import generic
+
 from .models import Post
 
 
-def home(request):
-	posts = Post.objects.all()
-	context = {'posts': posts}
-	return render(request, 'blog/home.html', context)
+# func views
+# def home(request):
+# 	posts = Post.objects.all()
+# 	context = {'posts': posts}
+# 	return render(request, 'blog/home.html', context)
 
 
 def about(request):
 	return render(request, 'blog/about.html')
+
+
+# generic views
+class PostListView(generic.ListView):
+	model = Post
+	template_name = 'blog/home.html'  # default: <app>/<model>_<viewtype>.html
+	context_object_name = 'posts'  # default: <object_list>
+	ordering = ['-data_posted']
+
+
+class PostDetailView(generic.DetailView):
+	model = Post
+	template_name = 'blog/post_detail.html'
+	context_object_name = 'post_detail'
+
