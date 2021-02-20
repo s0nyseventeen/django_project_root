c7ff243bf21c6cc77821cc2475baa208d8151f81
s0nyseventeen <sonysevent@gmail.com>
2021-02-20 21:57:04 +0200

    20/02 Pagination - OK

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 # LoginRequiredMixin with classes like decorators:@login_required() with func
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.auth.models import User
 
 from .models import Post
 
@@ -25,6 +26,20 @@ class PostListView(generic.ListView):
 	template_name = 'blog/home.html'  # default: <app>/<model>_<viewtype>.html
 	context_object_name = 'posts'  # default: <object_list>
 	ordering = ['-data_posted']
+	paginate_by = 10
+
+
+class UserPostListView(generic.ListView):
+	model = Post
+	template_name = 'blog/user_posts.html'
+	context_object_name = 'posts'
+	paginate_by = 10
+
+	# override model queryset(what we want to display)
+	def get_queryset(self):
+		# get a username from url if user exists
+		user = get_object_or_404(User, username=self.kwargs.get('username'))  
+		return Post.objects.filter(author=user).order_by('-data_posted')
 
 
 class PostDetailView(generic.DetailView):
@@ -38,7 +53,8 @@ class PostCreateView(LoginRequiredMixin, CreateView):
 	model = Post
 	fields = ['title', 'content']
 	template_name = 'blog/post_creat.html'
-	# success_url = reverse_lazy('blog:blog-home')  # if we don't user get_absolute_url() within model
+	# if we don't user get_absolute_url() within model
+	# success_url = reverse_lazy('blog:blog-home')  
 
 	def form_valid(self, form):
 		# form you're trying to submit, before you do that: take instance -> 
