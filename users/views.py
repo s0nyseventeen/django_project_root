2116c8aff47e15e166d3455f96a6909e5fe9a96a
s0nyseventeen <sonysevent@gmail.com>
2021-02-20 02:17:50 +0200

    20/02 Update Profile(image), User - OK

--- users/views.py
+++ users/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib import messages  # message: debug, info, success, warning, error
 from django.contrib.auth.decorators import login_required
 
-from .forms import UserRegisterForm
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 
 
 def register(request):
@@ -20,4 +20,21 @@ def register(request):
 
 @login_required
 def profile(request):
-	return render(request, 'users/profile.html')
+	if request.method == 'POST':
+		# (instance=request.user) to show old data in fields before changing
+		user_form = UserUpdateForm(request.POST, instance=request.user)
+		profile_form = ProfileUpdateForm(
+			request.POST,
+			request.FILES,
+			instance=request.user.profile
+		)
+		if user_form.is_valid() and profile_form.is_valid():
+			user_form.save()
+			profile_form.save()
+			messages.success(request, 'Your account has been updated')
+			return redirect('profile')
+	else:
+		user_form = UserUpdateForm(instance=request.user)
+		profile_form = ProfileUpdateForm(instance=request.user.profile)
+	context = {'user_form': user_form, 'profile_form': profile_form}
+	return render(request, 'users/profile.html', context)
